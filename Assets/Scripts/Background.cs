fbe9107ee15999d27065633198ea6371f154e7f3
Florin <flesteaf@gmail.com>
2020-05-24 22:57:16 +0300

    the background is now resizable based on screen size

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Background : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-    }
+        Image backgroundImage = GetComponentInChildren<Image>();
+        RectTransform backgroundRectTransform = backgroundImage.GetComponent<RectTransform>();
+        Rect canvasRect = transform.GetComponent<RectTransform>().rect;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        backgroundRectTransform.offsetMax = new Vector2(canvasRect.xMax, canvasRect.yMax);
+        backgroundRectTransform.offsetMin = new Vector2(canvasRect.xMin, canvasRect.yMin);
     }
 }
